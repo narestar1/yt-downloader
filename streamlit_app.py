4fcd7001f8c1a49665ab599cc56d3cde0bbae650
Naresh Babu Kalakada <naresh.kalakada@gmail.com>
2024-06-22 17:28:13 -0400

    added pytube to requirments.txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from pytube import YouTube
 import os
 
+
 def download_video(url):
     try:
         yt = YouTube(url)
@@ -10,6 +11,15 @@ def download_video(url):
         return f"downloads/{stream.default_filename}", yt.title
     except Exception as e:
         return None, f"Error: {e}"
+
+
+def delete_file(file_path):
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        return f"File {file_path} deleted."
+    else:
+        return f"File {file_path} not found."
+
 
 # Streamlit UI
 st.title("YouTube Video Downloader")
@@ -22,14 +32,15 @@ if st.button("Download"):
         if file_path:
             with open(file_path, "rb") as file:
                 btn = st.download_button(
-                    label="Download Video", 
+                    label="Download Video",
                     data=file,
                     file_name=os.path.basename(file_path),
-                    mime="video/mp4"
+                    mime="video/mp4",
+                    on_click=delete_file,
+                    args=(file_path,),
                 )
             st.success(f"Downloaded: {message}")
         else:
             st.error(message)
     else:
         st.error("Please provide a URL.")
-
